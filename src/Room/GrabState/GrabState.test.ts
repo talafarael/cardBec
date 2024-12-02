f1b9884afdfc8faa5840dc3d3d9229692e103159
artem <127351345+talafarael@users.noreply.github.com>
2024-12-02 10:01:05 +0200

    Update GrabState.test.ts

--- src/Room/GrabState/GrabState.test.ts
+++ src/Room/GrabState/GrabState.test.ts
@@ -1,22 +1,28 @@
 import { IRoom } from "../../Room";
 import { GrabState } from "./GrabState";
 
-const roomGrabFalse: IRoom = {
-  GrabState: false,
-} as unknown as IRoom;
 
-const roomGrabTrue: IRoom = {
-  GrabState: true,
-} as unknown as IRoom;
+
 describe("checkGrabState", () => {
   it("should return false", () => {
     const grabState = new GrabState();
+    
+const roomGrabFalse: IRoom = {
+    GrabState: false,
+  } as unknown as IRoom;
     expect(grabState.checkGrabState(roomGrabFalse)).toBe(false);
   });
 });
 
 describe("cheng to true", () => {
   it("should return true", () => {
+    const roomGrabFalse: IRoom = {
+        GrabState: false,
+      } as unknown as IRoom;
+      
+      const roomGrabTrue: IRoom = {
+        GrabState: true,
+      } as unknown as IRoom;
     const grabState = new GrabState();
     expect(grabState.changeGrabStateTrue(roomGrabFalse)).toEqual(roomGrabTrue);
   });
@@ -24,7 +30,15 @@ describe("cheng to true", () => {
 
 describe("cheng to false", () => {
   it("should return false", () => {
+    const roomGrabFalse: IRoom = {
+        GrabState: false,
+      } as unknown as IRoom;
+      
+      const roomGrabTrue: IRoom = {
+        GrabState: true,
+      } as unknown as IRoom;
     const grabState = new GrabState();
+    
     expect(grabState.changeGrabStateFalse(roomGrabTrue)).toEqual(roomGrabFalse);
   });
 });
