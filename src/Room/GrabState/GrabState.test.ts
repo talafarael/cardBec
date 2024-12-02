07c4d7ec1ac1be020c730aa0f725f8c304a1ff5c
artem <127351345+talafarael@users.noreply.github.com>
2024-12-02 09:57:13 +0200

    test grab state

---
+++ src/Room/GrabState/GrabState.test.ts
@@ -0,0 +1,30 @@
+import { IRoom } from "../../Room";
+import { GrabState } from "./GrabState";
+
+const roomGrabFalse: IRoom = {
+  GrabState: false,
+} as unknown as IRoom;
+
+const roomGrabTrue: IRoom = {
+  GrabState: false,
+} as unknown as IRoom;
+describe("checkGrabState", () => {
+  it("should return false", () => {
+    const grabState = new GrabState();
+    expect(grabState.checkGrabState(roomGrabFalse)).toBe(false);
+  });
+});
+
+describe("cheng to true", () => {
+  it("should return true", () => {
+    const grabState = new GrabState();
+    expect(grabState.changeGrabStateTrue(roomGrabFalse)).toEqual(roomGrabTrue);
+  });
+});
+
+describe("cheng to false", () => {
+  it("should return false", () => {
+    const grabState = new GrabState();
+    expect(grabState.changeGrabStateFalse(roomGrabTrue)).toEqual(roomGrabFalse);
+  });
+});
